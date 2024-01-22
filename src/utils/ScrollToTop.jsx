962c0fc1cb74d20254d217fbdd3429db48ac6cd2
bao truong <baothaitruong2000@gmail.com>
2024-01-22 13:50:57 +0700

    init

---
+++ src/utils/ScrollToTop.jsx
@@ -0,0 +1,10 @@
+import {useEffect}  from 'react';
+
+const ScrollToTop = ({ children }) => {
+    useEffect(() => {
+        window.scrollTo({top: 0, behavior: 'smooth'});
+    });
+    return children;
+};
+
+export default ScrollToTop;
